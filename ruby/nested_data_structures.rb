237f2e6bbacd085091a16eacded785f57f4f7990
kellitjones <jonestkelli@gmail.com>
2016-08-14 21:05:21 -0500

    design and build a nested data structure to represent a real-world construct

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,73 @@
+sneaker_collection = {
+  nikes:{
+    collection_info:{
+      total_pairs:17,
+      high_tops:3,
+      low_tops:14
+    },
+    colors:{
+      black:2,
+      blue:2,
+      green:6,
+      grey:6,
+      white:0,
+      other:1
+    },
+    names: [
+    "dunk",
+    "lunar force",
+    "huarache",
+    "air max"]
+  },
+
+  adidas:{
+    collection_info:{
+      total_pairs:5,
+      high_tops:2,
+      low_tops:3,
+    },
+    colors:{
+      black:0,
+      blue:2,
+      green:1,
+      grey:1,
+      white:1,
+      other:0
+    },
+    names: [
+    "d rose 3.5",
+    "forum",
+    "crazylight boost",
+    "superstar"]
+  },
+
+  vans:{
+    collection_info:{
+      total_pairs:7,
+      high_tops:5,
+      low_tops:2,
+    },
+    colors:{
+      black:1,
+      blue:2,
+      green:1,
+      grey:1,
+      white:0,
+      other:2,
+    },
+    names: [
+      "sk8-hi",
+      "106",
+      "half cab",
+      "old skool"]
+  }
+}
+
+#returns the last item in the vans, name collection
+sneaker_collection[:vans][:names][3]
+
+#returns the nike names collection in reverse
+sneaker_collection[:nikes][:names].reverse
+
+#returns the adidas name collection with another shoe name added, 'jabbar'
+sneaker_collection[:adidas][:names].push("jabbar")
