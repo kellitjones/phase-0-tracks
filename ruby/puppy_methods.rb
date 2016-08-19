df258b2de57548399b92099ea53781dfdf16f1c1
kellitjones <jonestkelli@gmail.com>
2016-08-19 16:29:51 -0500

    finish release zero

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(num)
+    puts "Woof!" * num
+  end
+
+  def roll_over
+    puts "*rolls over*"
+  end
+
+  def dog_years(human_years)
+  puts human_years * 7
+  end
+
+  def chase_cat
+  puts "I'm gonna get you!"
+  end
+
 end
 
+spot = Puppy.new
+spot.fetch("ball")
+spot.speak(2)
+spot.roll_over
+spot.dog_years(3)
+spot.chase_cat
