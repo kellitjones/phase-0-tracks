f92eec71319d4dd10547eeea61fe3f16f611c302
kellitjones <jonestkelli@gmail.com>
2016-08-20 14:19:18 -0500

    edit challenges to make tests pass

--- ruby/list/todo_list_spec.rb
+++ ruby/list/todo_list_spec.rb
@@ -1,7 +1,9 @@
+require_relative 'todo_list'
+
 describe TodoList do
   let(:list) { TodoList.new(["do the dishes", "mow the lawn"]) }
 
-  it "stores the list items given on initialization" do
+  it "initializes with two items" do
     expect(list.get_items).to eq ["do the dishes", "mow the lawn"]
   end
 
