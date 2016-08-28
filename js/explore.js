859e5a8e7689c804fcf83f43d7314b5747018cf5
kellitjones <jonestkelli@gmail.com>
2016-08-28 17:09:29 -0400

    write a program to reverse a string

---
+++ js/explore.js
@@ -0,0 +1,18 @@
+// Create a function with one parameter
+// The one parameter will be a string
+// reverse the string parameter by splitting string in single letters (.split)
+// Then reverse the order of the strings letters (.reverse)
+// Next join the letters back together using (.join)
+// Text the function
+
+var num = 1
+
+function Tune(song){
+  var reveresedSong = song.split('').reverse().join('');
+  if (num == 1){
+  console.log(reveresedSong)
+  } else {
+    console.log("Nope!")
+  }
+}
+Tune("Babe I got you babe")
